0f9a5a7edae0dcc8e84c5eb6e3664a1d89c256dd
AchieBee <belza.achieng@student.moringaschool.combelza.achieng@student.moringaschool.com>
2023-08-11 13:14:45 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+  //console.log(`I'm ${age} years old. Happy birthday to me!`);
+  //debugger;
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts){
+//for(let i =0; i<gifts.length; i++){
+  //  console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+//}
+//return gifts;
+//}
+//wrapGifts(gifts)
+
+let names = ['Charles', 'Samip', 'Ali']
+let eventName = 'birthday'
+
+function writeCards(names, eventName){
+    for (let i=0; i <names.length; i++){
+        console.log(`Thank you ${names[i]}, for the wonderful ${eventName} gift!`);
+        //debbuger;
+    }
+    return names, eventName;
+}
+writeCards(names, eventName)
